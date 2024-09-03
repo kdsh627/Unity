9ffb4d73befabeed1f757f53e263938834e34552
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2024-09-03 12:22:06 +0900

    Update Test.cs

--- DeliveryDriver/Assets/Test.cs
+++ DeliveryDriver/Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("aaa");
     }
 
     // Update is called once per frame
